5a5146b75a2b2627923130d0555a72e073773cab
James Darwent <jamesdarwent_88@hotmail.co.uk>
2021-01-16 18:42:33 +0000

    Naming completed

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,7 +1,12 @@
 const Pet = require("../src/pet");
 
 describe("constructor", () => {
-    test("returns an object", () => {
-        expect(new Pet("Biscuit")).toBeInstanceOf(Object);
+    it("returns an object", () => {
+        expect(new Pet("Fido")).toBeInstanceOf(Object);
+    });
+    it("sets the name property", () => {
+        const pet = new Pet("Fido");
+
+        expect(pet.name).toEqual("Fido");
     });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,6 +1,12 @@
-function Pet() {
-    
-}
+function Pet(name) {
+    this.name = name;
+};
+
+const pet = {
+    name: "Fido"
+};
+
+
 
 module.exports = Pet;
 
