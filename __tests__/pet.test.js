c87ca2a579b43bebe68847f512ae1c3679ef5941
James Darwent <jamesdarwent_88@hotmail.co.uk>
2021-01-19 15:36:50 +0000

    Play method added + tests written for walking method to influence happiness

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,25 +4,35 @@ describe("constructor", () => {
     it("returns an object", () => {
         expect(new Pet("Biscuit")).toBeInstanceOf(Object);
     });
+
     it("sets the name property", () => {
         const pet = new Pet("Biscuit");
 
         expect(pet.name).toEqual("Biscuit");
     });
+
     it("has an initial age of 0", () => {
         const pet = new Pet("Biscuit");
 
         expect(pet.age).toEqual(0);
     });
+
     it("has an initial hunger of 0", () => {
         const pet = new Pet("Biscuit");
 
         expect(pet.hunger).toEqual(0);
     });
+
     it("has an initial fitness of 10", () => {
         const pet = new Pet("Biscuit");
 
         expect(pet.fitness).toEqual(10);
+    });
+
+    it("has initial happiness of 10", () =>  {
+        const pet = new Pet("Biscuit");
+
+        expect(pet.happiness).toEqual(10);
     });
 });
 
@@ -35,6 +45,7 @@ describe("growUp", () => {
 
         expect(pet.age).toEqual(1);
     });
+
     it("increases hunger by 5", () => {
         const pet = new Pet("Biscuit");
 
@@ -42,6 +53,7 @@ describe("growUp", () => {
 
         expect(pet.hunger).toEqual(5);
     });
+
     it("decreases fitness by 3", () => {
         const pet = new Pet("Biscuit");
 
@@ -49,6 +61,15 @@ describe("growUp", () => {
 
         expect(pet.fitness).toEqual(7);
     });
+
+    it("decreases happiness by 5", () => {
+        const pet = new Pet("Biscuit");
+
+        pet.growUp();
+
+        expect(pet.happiness).toEqual(5);
+    });
+
     it("throws an error if pet is no longer alive", () => {
         const pet = new Pet("Biscuit");
 
@@ -68,6 +89,7 @@ describe("walk", () => {
 
         expect(pet.fitness).toEqual(8);
     });
+
     it("increases fitness to a maximum of 10", () => {
         const pet = new Pet("Biscuit");
 
@@ -76,6 +98,25 @@ describe("walk", () => {
 
         expect(pet.fitness).toEqual(10);
     });
+
+    xit("increases happiness by 1", () => {
+        const pet = new Pet("Biscuit");
+
+        pet.happiness = 3;
+        pet.walk();
+
+        expect(pet.happiness).toEqual(4);
+    });
+
+    xit("increases happiness to a maximum of 10", () =>{
+        const pet = new Pet("Biscuit");
+
+        pet.happiness = 10;
+        pet.walk();
+
+        expect(pet.happiness).toEqual(10);
+    });
+
     it("throws an error if pet is no longer alive", () => {
         const pet = new Pet("Biscuit");
 
@@ -95,6 +136,7 @@ describe("feed", () => {
 
         expect(pet.hunger).toEqual(6);
     });
+
     it("decreases hunger to a minimum of 0", () => {
         const pet = new Pet("Biscuit");
 
@@ -103,12 +145,42 @@ describe("feed", () => {
 
         expect(pet.hunger).toEqual(0);
     });
+
     it("throws an error if pet is no longer alive", () => {
         const pet = new Pet("Biscuit");
 
         pet.hunger = 10;
 
         expect(() => pet.feed()).toThrow("R.I.P Biscuit. A pet is for life, not just for Christmas");
+    });
+});
+
+
+describe("play", () => {
+    it("increases happiness by 3", () => {
+        const pet = new Pet("Biscuit");
+
+        pet.happiness = 5;
+        pet.play();
+
+        expect(pet.happiness).toEqual(8);
+    });
+
+    it("increases happiness to a maximum of 10", () => {
+        const pet = new Pet("Biscuit");
+
+        pet.happiness = 9;
+        pet.play();
+
+        expect(pet.happiness).toEqual(10);
+    });
+
+    it("throws an error if pet is no longer alive", () => {
+        const pet = new Pet("Biscuit");
+
+        pet.happiness = 0;
+
+        expect(() => pet.play()).toThrow("R.I.P Biscuit. A pet is for life, not just for Christmas");
     });
 });
 
@@ -122,6 +194,7 @@ describe("checkUp", () => {
 
         expect(pet.checkUp()).toEqual("I need a walk");
     });
+
     it("if hunger is 5 or more, return I am hungry", () => {
         const pet = new Pet("Biscuit");
 
@@ -130,24 +203,68 @@ describe("checkUp", () => {
 
         expect(pet.checkUp()).toEqual("I am hungry");
     });
-    it("if both of the above are true, return I am hungry and I need a walk", () => {
+
+    it("if happiness is 5 or less, returns I feel lonely", () => {
+        const pet = new Pet("Biscuit");
+
+        pet.happiness = 5;
+        pet.checkUp();
+
+        expect(pet.checkUp()).toEqual("I feel lonely :(");
+    });
+
+    it("if hunger is high and fitness is low, returns I am hungry and I need a walk", () => {
         const pet = new Pet("Biscuit");
 
         pet.fitness = 1;
         pet.hunger = 5;
         pet.checkUp();
 
-        expect(pet.checkUp()).toEqual("I am hungry AND I need a walk");
+        expect(pet.checkUp()).toEqual("I am hungry and I need a walk");
     });
+
+    it("if hunger is high and happiness is low, returns I feel hungry and lonely", () => {
+        const pet = new Pet("Biscuit");
+
+        pet.hunger = 5;
+        pet.happiness = 2;
+        pet.checkUp();
+
+        expect(pet.checkUp()).toEqual("I feel hungry and lonely");
+    });
+
+    it("if happiness and fitness are low, returns I feel lonely and need a walk", () => {
+        const pet = new Pet("Biscuit");
+
+        pet.fitness = 1;
+        pet.happiness = 3;
+        pet.checkUp();
+
+        expect(pet.checkUp()).toEqual("I feel lonely and need a walk");
+    });
+
+    it("if all of the above are true, return I am hungry, I need a walk and I feel lonely", () => {
+        const pet = new Pet("Biscuit");
+
+        pet.fitness = 1;
+        pet.hunger = 5;
+        pet.happiness = 4;
+        pet.checkUp();
+
+        expect(pet.checkUp()).toEqual("I feel lonely, hungry, and I need a walk");
+    });
+
     it("if neither of the above are true, return I feel great", () => {
         const pet = new Pet("Biscuit");
 
         pet.fitness = 4;
         pet.hunger = 3;
+        pet.happiness = 7;
         pet.checkUp();
 
         expect(pet.checkUp()).toEqual("I feel great!");
     });
+
     it("throws an error if pet is no longer alive", () => {
         const pet = new Pet("Biscuit");
 
@@ -159,14 +276,15 @@ describe("checkUp", () => {
 
 
 describe("isAlive", () => {
-    it("returns false if fitness is 0 or less, & if hunger is 10 or more, & if age is 30 or more", () => {
+    it("returns false if pet is no longer alive", () => {
         const pet = new Pet("Biscuit");
 
         pet.age = 31;
         
         expect(pet.isAlive).toEqual(false);
     });
-    it("returns ture if none of the above are true", () => {
+
+    it("returns ture if pet is alive", () => {
         const pet = new Pet("Biscuit");
 
         pet.age = 28;
@@ -174,5 +292,3 @@ describe("isAlive", () => {
         expect(pet.isAlive).toEqual(true);
     });
 });
-
-
--- src/pet.js
+++ src/pet.js
@@ -2,26 +2,39 @@
 const birth_age = 0;
 const birth_hunger = 0;
 const birth_fitness = 10;
+const birth_happiness = 10;
 
 // Growing up
 const age_increment = 1;
 const hunger_increment = 5;
 const fitness_decrement = 3;
+const happiness_decrement = 5;
 
 // Walking
 const fitness_increment = 4;
+const walking_happiness_increment = 1;
 
 // Feeding
 const hunger_decrement = 3;
 
+// Playing
+const happiness_increment = 3;
+
 // Hunger & Fitness warnings
-const getting_hungry = 5;
-const getting_weak = 3;
+const feeling_hungry = 5;
+const feeling_weak = 3;
+const feeling_lonely = 5;
+
+// Min / Max values
+const minimum_hunger = 0;
+const maximum_fitness = 10;
+const maximum_happiness = 10;
 
 // Death values
 const age_limit = 30;
 const hunger_limit = 10;
 const fitness_limit = 0;
+const happiness_limit = 0;
 
 // Death message
 const RIP1 = "R.I.P" // Followed by pet's name
@@ -33,11 +46,12 @@ function Pet(name) {
     this.age = birth_age;
     this.hunger = birth_hunger;
     this.fitness = birth_fitness;
+    this.happiness = birth_happiness;
 };
 
 Pet.prototype = {
     get isAlive() {
-        return this.age < age_limit && this.hunger < hunger_limit && this.fitness > fitness_limit;
+        return this.age < age_limit && this.hunger < hunger_limit && this.fitness > fitness_limit && this.happiness > happiness_limit;
     }
 };
 
@@ -48,39 +62,65 @@ Pet.prototype.growUp = function() {
     this.age += age_increment;
     this.hunger += hunger_increment;
     this.fitness -= fitness_decrement;
+    this.happiness -= happiness_decrement;
 };
 
 Pet.prototype.walk = function() {
     if(!this.isAlive) {
         throw new Error (`${RIP1} ${this.name}. ${RIP2}`);
-    } else if((this.fitness + fitness_increment) <= birth_fitness) {
+    } else if((this.fitness + fitness_increment) <= maximum_fitness) {
         this.fitness += fitness_increment;
     } else {
-        this.fitness = birth_fitness;
+        this.fitness = maximum_fitness;
     };
 };
 
 Pet.prototype.feed = function () {
     if(!this.isAlive) {
         throw new Error (`${RIP1} ${this.name}. ${RIP2}`);
-    } else if((this.hunger - hunger_decrement) >= birth_hunger) {
+    } else if((this.hunger - hunger_decrement) >= minimum_hunger) {
         this.hunger -= hunger_decrement;
     } else {
-        this.hunger = birth_hunger;
+        this.hunger = minimum_hunger;
+    };
+};
+
+Pet.prototype.play = function () {
+    if (!this.isAlive) {
+        throw new Error (`${RIP1} ${this.name}. ${RIP2}`);
+    } else if ((this.happiness + happiness_decrement) <= maximum_happiness) {
+        this.happiness += happiness_increment;
+    } else {
+        this.happiness = maximum_happiness;
     };
 };
 
 Pet.prototype.checkUp = function() {
     if(!this.isAlive) { return (`${RIP1} ${this.name}. ${RIP2}`);
     };
-    if((this.fitness <= getting_weak) && (this.hunger >= getting_hungry)) {
-        return "I am hungry AND I need a walk";
+    if((this.fitness <= feeling_weak) && (this.hunger >= feeling_hungry) && (this.happiness <= feeling_lonely)) {
+        return "I feel lonely, hungry, and I need a walk";
     };
-    if(this.fitness <= getting_weak) {
+    if((this.fitness <= feeling_weak) && (this.hunger >= feeling_hungry)) {
+        return "I am hungry and I need a walk";
+    };
+    if((this.fitness <= feeling_weak) && (this.hunger >= feeling_hungry)) {
+        return "I am hungry and I need a walk";
+    };
+    if((this.hunger >= feeling_hungry) && (this.happiness <= feeling_lonely)) {
+        return "I feel hungry and lonely";
+    };
+    if((this.happiness <= feeling_lonely) && (this.fitness <= feeling_weak)) {
+        return "I feel lonely and need a walk";
+    };
+    if(this.fitness <= feeling_weak) {
         return "I need a walk";
     };
-    if(this.hunger >= getting_hungry) {
+    if(this.hunger >= feeling_hungry) {
         return "I am hungry";
+    };
+    if(this.happiness <= feeling_lonely) {
+        return "I feel lonely :(";
     } else {
         return "I feel great!";
     };   
